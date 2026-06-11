d68cdfc5888e390c9d18aa0e087ad07b6e0b91d1
mkey91566-art <mkey91566@gmail.com>
2026-06-11 09:18:23 +0630

    Add RSS feeds for football news

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Football News Bot Started")
+rss_feeds = [
+    "https://feeds.bbci.co.uk/sport/football/rss.xml",
+    "https://www.skysports.com/rss/12040",
+    "https://news.google.com/rss/search?q=football"
+]
